9a1beee31beec5c7af9cb67d2049300dd82875de
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-21 23:45:26 +0100

    oppdatering

--- Boksøk-app/src/components/Searchresults.jsx
+++ Boksøk-app/src/components/Searchresults.jsx
@@ -1,1 +1,17 @@
 import { useState } from "react"
+
+export default function Searchresults({content,query})
+
+return(
+    <>
+    <section>
+    <h1>{query.toUpperCase().replaceAll("+","")}</h1>
+    <div className="searchResultsContent">
+    {content?.map((book,index) =>
+      <Bookcard key={"book"+ index} title={book.title} author={book.author} 
+      year={book.first_publish_year} rating={book.ratings_average} amazoneId={book.id_amazon} 
+       image={book.cover_edition_key} searchindex={index}/>)}
+    </div>
+    </section>
+    </>
+)
