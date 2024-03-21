888c19b50da2c09253a07d78fb9de736c457ec23
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-21 22:12:55 +0100

    oppdatering av -bookcard.scss
    oppdatering av -bookcard.scss

--- Boksøk-app/src/App.jsx
+++ Boksøk-app/src/App.jsx
@@ -7,9 +7,35 @@ import BookPage from './components/BookPage'
 
 function App() {
   
+  const [content,setContent] = useState([])
+  const [query,setQuery] = useState(["James+Bond"])
+
+  const getData = async() => {
+    try{
+      const response = await fetch('https://openlibrary.org/search.json?title=${query}&limit=20')
+      const data = await response.json()
+      setContent(data.docs)
+      localStorage.setItem('queryData', JSON.stringify(data.docs))
+    } catch{
+      console.log("error i getDat funksjon")
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [query])
 
   return (
     <>
+    <Layout setQuery={setQuery}>
+      <Routes>
+        <Route index element={<Searchresults content={content} query={query}/>}/>
+      </Routes>
+      
+        <Routes>
+          <Route path='/book/:id' element={<BookPage />}/>}/>
+        </Routes>
+      
+    </Layout>
       
     </>
   )
