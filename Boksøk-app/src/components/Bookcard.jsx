40e8bc4fa14f4fd67226b5e82f5df9dccf5b464b
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-22 23:05:14 +0100

    BooCard jsx og scss oppdatert

--- Boksøk-app/src/components/Bookcard.jsx
+++ Boksøk-app/src/components/Bookcard.jsx
@@ -1,13 +1,39 @@
-import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 
 
 
-export default function BookCard({}){
+export default function BookCard({title, year,author, rating, amazoneId, image, searchindex}){
 
     return(
-        <article>
-            
+        <article className="articleCardBody">
+            <div className="articleCard">
+              {image?(<img src={`https://covers.openlibrary.org/b/olid/${image}-M.jpg`} 
+              alt={`Coverbilde for ${title}`}/>) : (<img src={"https://i.imgur.com/0ZzYCzH.png"} 
+              alt={`Fant ingen forsidebilde for denne boken`}/>)}<div>
+            <h2>{title}</h2>
+            <h3>Forfatter(s):</h3>
+            <ul className="authorListarticleCardAuhorList">
+                {author?.slice(0,3).map((item, index) => index > 1?
+                (<li key={"author" + index}>
+                <Link to={`/book/:${searchindex}`}>and more</Link>
+            </li>)
+        :
+            (<li key={"author" + index}>{item}</li>))}
+            </ul>
+            <h4>Først publisert</h4>
+            <p>{year}</p> 
+            {rating? (<><h4>Rating:</h4> <p>{rating?.toFixed(1)}</p></>) : (<><h4>Rating:</h4> <p>none</p></>)}
+            <h4>Amazon links:</h4>
+            <ul className="articleCardAmazonList">
+            <li><Link to={`https://www.amazon.com/s?k=${title}`}>Link 1</Link></li>
+                {amazoneId?.slice(0, 3).map((item, idx) => 
+            <li key={"amazonlink"+idx}><Link key={"amazonLink"+idx} to={`https://www.amazon.com/s?k=${item}`}>Link {idx+2}</Link></li>
+            )}
+            </ul>
+            </div>
+            </div>
+            <Link to={`/book/:${searchindex}`} className="readMore">Les mer</Link>
+
         </article>
 
     )
